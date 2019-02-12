385c08f7539a207e0d85bc14096a4bd45e3f131a
Valerie S <43964677+vs0018@users.noreply.github.com>
2019-02-12 12:12:52 -0500

    add dotenv files and require all needed modules

---
+++ bamazonCustomer.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+
+var mysql = require("mysql");
+
+var myPass = require("./pass.js");
+
+var newSQL = new SQL(myPass.pass);
+
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: newSQL.password,
+    database: "bamazon"
+  });
+  
+  connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
