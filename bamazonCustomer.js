5f61bf4e6837cd9342e39026c87b366ed56fb940
Valerie S <43964677+vs0018@users.noreply.github.com>
2019-02-12 23:33:49 -0500

    try to fix raw list in inquirer prompt

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -22,7 +22,6 @@ connection.connect(function(err) {
     if (err) throw err;
 
     start();
-
 });
 
 function start() {
@@ -39,6 +38,76 @@ function start() {
                 console.log("------------------------");
             };
         };
-
     });
+
+    buyItem();
+};
+
+function buyItem() {
+
+    connection.query("SELECT * FROM products", function(err, results) {
+        if (err) throw err;
+        
+        console.log (results);
+        
+    inquirer
+    .prompt([
+    {
+        name: "choice",
+        type: "rawlist",
+        choices: function(results) {
+        var choiceArray = [];
+        for (var i = 0; i < results.length; i++) {
+            choiceArray.push(results[i].product_name);
+        }
+        return choiceArray;
+        },
+        message: "What would you like to buy?"
+    },
+    {
+        name: "buy",
+        type: "input",
+        message: "How many units would you like to buy?"
+    }
+    ])
+    .then(function(answer) {
+        // get the information of the chosen item
+        var chosenItem;
+        for (var i = 0; i < results.length; i++) {
+            if (results[i].product_name === answer.choice) {
+            chosenItem = results[i];
+            }
+        }
+
+        // determine if there is enough stock to fulfill order
+        if (chosenItem.stock_quantity < parseInt(answer.buy)) {
+        
+            // stock high enough, so update db, let the user know, and start over
+            connection.query(
+            "UPDATE products SET ? WHERE ?",
+            [
+                {
+                stock_quantity: chosenItem.stock_quantity - answer.buy
+                },
+                {
+                id: chosenItem.id
+                }
+            ],
+            function(error) {
+                if (error) throw err;
+                console.log("Order placed successfully!");
+                start();
+            }
+            );
+        }
+        else {
+            // stock wasn't high enough, so apologize and start over
+            console.log("Sorry, not enough stock to fulfill your order. Please try again...");
+            start();
+        }
+        });
+    });
+
+
+
 };
